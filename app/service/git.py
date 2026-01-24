f929ee88307cbeaa949072b8bbfcb016c2f4195b
kcepu877 <kcepu877@gmail.com>
2026-01-25 03:38:38 +0700

    Update git.py
    Signed-off-by: kcepu877 <kcepu877@gmail.com>

--- app/service/git.py
+++ app/service/git.py
@@ -13,7 +13,7 @@ from app2.menus.util import print_error, print_success, print_warning, print_pan
 console = Console()
 
 OWNER = "kcepu877"
-REPO = "xr"
+REPO = "XR"
 BRANCH = "main"
 EXPECTED_URL = f"https://github.com/{OWNER}/{REPO}"
 
